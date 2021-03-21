750cec1f6fdd4d2e446537cd327a5a5ea5b8777b
Jim Nguyen <jimnguyen220@gmail.com>
2021-03-20 20:57:10 -0500

    configured server connection

---
+++ server.js
@@ -0,0 +1,21 @@
+const express = require('express');
+const exphbs = require('express-handlebars');
+// const routes = require('./controllers');
+// const helpers = require('./utils/helpers');
+const sequelize = require('./config/connection');
+
+const app = express();
+const PORT = process.env.PORT || 3001;
+
+const hbs = exphbs.create({});
+
+app.engine('handlebars', hbs.engine);
+app.set('view engine', 'handlebars');
+
+app.use(express.json());
+app.use(express.urlencoded({extended: true}));
+app.use(express.static("public"));
+
+// app.use(routes);
+
+app.listen(PORT, () => console.log('Now Listening'));
