56a2710d2609a2532ad29fc7197e42342228279d
Jim Nguyen <jimnguyen220@gmail.com>
2021-04-04 08:34:09 -0500

    debugging, changed PORT on server

--- server.js
+++ server.js
@@ -8,7 +8,7 @@ const sequelize = require('./config/connection');
 const SequelizeStore = require('connect-session-sequelize')(session.Store);
 
 const app = express();
-const PORT = process.env.PORT || 3001;
+// const PORT = process.env.PORT || 3001;
 
 const hbs = exphbs.create({ helpers });
 
@@ -39,5 +39,7 @@ app.use(express.static("public"));
 app.use(routes);
 
 sequelize.sync({force: false}).then(() => {
-    app.listen(PORT, () => console.log('Now Listening'));
+    app.listen(process.env.PORT || 3000, function(){
+        console.log("Express server listening on port %d in %s mode", this.address().port, app.settings.env);
+      });
 });
